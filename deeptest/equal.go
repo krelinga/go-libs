b591533a5c5afec6c381e78891ec5e395bccb0aa
Andy Kreling <krelinga@gmail.com>
2025-11-07 08:27:03 +0000

    add env.Helper() calls.

--- deeptest/equal.go
+++ deeptest/equal.go
@@ -3,11 +3,13 @@ package deeptest
 import "github.com/krelinga/go-deep/match"
 
 func Equal[T any](env Env, a, b T) Result {
+	env.Helper()
 	m := match.Equal(b)
 	return Match(env, a, m)
 }
 
 func NotEqual[T any](env Env, a, b T) Result {
+	env.Helper()
 	m := match.NotEqual(b)
 	return Match(env, a, m)
 }
