bbec279b2f8114b615b3ed84ddd11f4e4f49c3ef
Andy Kreling <krelinga@gmail.com>
2025-11-07 07:13:46 +0000

    Substring().

---
+++ match/substr.go
@@ -0,0 +1,23 @@
+package match
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/krelinga/go-deep"
+)
+
+func Substring(sub string) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		got := vals.Want1()
+		if got.Kind() != reflect.String {
+			panic(fmt.Errorf("%w: value is not a string: got %s", ErrBadType, got.Kind()))
+		}
+		gotString := got.String()
+		if strings.Contains(gotString, sub) {
+			return NewResult(true, fmt.Sprintf("string contains substring %q", sub))
+		}
+		return NewResult(false, fmt.Sprintf("string does not contain substring %q", sub))
+	})
+}
