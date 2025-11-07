ffd1d51cbe50c43c52004d98c3d73bf54fa1fd85
Andy Kreling <krelinga@gmail.com>
2025-11-07 05:25:42 +0000

    logical.go.

---
+++ match/logical.go
@@ -0,0 +1,49 @@
+package match
+
+import "github.com/krelinga/go-deep"
+
+func AllOf(matchers ...Matcher) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		for _, matcher := range matchers {
+			result := matcher.Match(env, vals)
+			if !result.Matched() {
+				return NewResult(false, "not all matchers matched: "+result.String())
+			}
+		}
+		return NewResult(true, "all matchers matched")
+	})
+}
+
+func AnyOf(matchers ...Matcher) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		for _, matcher := range matchers {
+			result := matcher.Match(env, vals)
+			if result.Matched() {
+				return NewResult(true, "at least one matcher matched: "+result.String())
+			}
+		}
+		return NewResult(false, "no matchers matched")
+	})
+}
+
+func Not(matcher Matcher) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		result := matcher.Match(env, vals)
+		if result.Matched() {
+			return NewResult(false, "matcher matched: "+result.String())
+		}
+		return NewResult(true, "matcher did not match: "+result.String())
+	})
+}
+
+func NoneOf(matchers ...Matcher) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		for _, matcher := range matchers {
+			result := matcher.Match(env, vals)
+			if result.Matched() {
+				return NewResult(false, "at least one matcher matched: "+result.String())
+			}
+		}
+		return NewResult(true, "no matchers matched")
+	})
+}
