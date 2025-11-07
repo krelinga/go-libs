f7e631f39e67f8c4e03827163509ae5f509ffbee
Andy Kreling <krelinga@gmail.com>
2025-11-07 06:47:31 +0000

    Interface().

---
+++ match/interface.go
@@ -0,0 +1,22 @@
+package match
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/krelinga/go-deep"
+)
+
+func Interface(m Matcher) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		got := vals.Want1()
+		if got.Kind() != reflect.Interface {
+			panic(fmt.Errorf("%w: value is not an interface: got %s", ErrBadType, got.Kind()))
+		}
+		if got.IsNil() {
+			return NewResult(false, "interface is nil")
+		}
+		elem := got.Elem()
+		return m.Match(env, Vals{elem})
+	})
+}
