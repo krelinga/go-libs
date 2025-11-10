fc2e4698a89eb108ee5cb264f90fc74a591d922e
Andy Kreling <krelinga@gmail.com>
2025-11-10 04:30:21 +0000

    NewT().
    Also split up TestingT{} into CommonT{} and TestingT{}.

--- deeptest/types.go
+++ deeptest/types.go
@@ -2,13 +2,13 @@ package deeptest
 
 import (
 	"context"
+	"testing"
 	"time"
 
 	"github.com/krelinga/go-deep"
 )
 
-// Methods from testing.T (other than Run()) are captured in this interface.
-type TestingT interface {
+type CommonT interface {
 	// TODO: add Attr()
 	Chdir(dir string)
 	Cleanup(f func())
@@ -35,9 +35,40 @@ type TestingT interface {
 	TempDir() string
 }
 
+type TestingT interface {
+	CommonT
+	Run(name string, f func(t *testing.T)) bool
+}
+
 // Similar to testing.T, but with deep.Env support.
 type T interface {
-	TestingT
+	CommonT
 	Run(name string, f func(T)) bool
 	DeepEnv() deep.Env
+}
+
+type tImpl struct {
+	TestingT
+	deepEnv deep.Env
+}
+
+func (t *tImpl) DeepEnv() deep.Env {
+	return t.deepEnv
+}
+
+func (t *tImpl) Run(name string, f func(T)) bool {
+	return t.TestingT.Run(name, func(tt *testing.T) {
+		subT := &tImpl{
+			TestingT: tt,
+			deepEnv:  deep.WrapEnv(t.deepEnv),
+		}
+		f(subT)
+	})
+}
+
+func NewT(t TestingT, env deep.Env) T {
+	return &tImpl{
+		TestingT: t,
+		deepEnv:  env,
+	}
 }
