9e488ceed66050a6a7b37002c9e0d046d0bb3fa6
Andy Kreling <krelinga@gmail.com>
2025-11-11 05:39:00 +0000

    order.go and order_test.go.

---
+++ exam/order.go
@@ -0,0 +1,30 @@
+package exam
+
+import (
+	"github.com/krelinga/go-deep"
+	"github.com/krelinga/go-deep/match"
+)
+
+func LessThan[Type any](e E, env deep.Env, a, b Type) Result {
+	e.Helper()
+	m := match.LessThan(b)
+	return Match(e, env, a, m)
+}
+
+func GreaterThan[Type any](e E, env deep.Env, a, b Type) Result {
+	e.Helper()
+	m := match.GreaterThan(b)
+	return Match(e, env, a, m)
+}
+
+func LessThanOrEqual[Type any](e E, env deep.Env, a, b Type) Result {
+	e.Helper()
+	m := match.LessThanOrEqual(b)
+	return Match(e, env, a, m)
+}
+
+func GreaterThanOrEqual[Type any](e E, env deep.Env, a, b Type) Result {
+	e.Helper()
+	m := match.GreaterThanOrEqual(b)
+	return Match(e, env, a, m)
+}
---
+++ exam/order_test.go
@@ -0,0 +1,253 @@
+package exam_test
+
+import (
+	"testing"
+
+	"github.com/krelinga/go-deep"
+	"github.com/krelinga/go-deep/exam"
+	"github.com/krelinga/go-deep/match"
+)
+
+func TestLessThan(t *testing.T) {
+	tests := []struct {
+		name    string
+		a, b    int
+		want    bool
+		matcher match.Matcher
+	}{
+		{
+			name: "a less than b",
+			a:    3,
+			b:    5,
+			want: true,
+			matcher: match.Equal(&exam.Log{
+				Helper: true,
+			}),
+		},
+		{
+			name: "a equal to b",
+			a:    5,
+			b:    5,
+			want: false,
+			matcher: match.Pointer(match.Struct{
+				Fields: map[deep.Field]match.Matcher{
+					deep.NamedField("Error"): match.Len(match.Equal(1)),
+				},
+				Partial: match.Equal(exam.Log{
+					Helper: true,
+					Fail:   true,
+				}),
+			}),
+		},
+		{
+			name: "a greater than b",
+			a:    7,
+			b:    5,
+			want: false,
+			matcher: match.Pointer(match.Struct{
+				Fields: map[deep.Field]match.Matcher{
+					deep.NamedField("Error"): match.Len(match.Equal(1)),
+				},
+				Partial: match.Equal(exam.Log{
+					Helper: true,
+					Fail:   true,
+				}),
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := exam.Harness{}
+			log := h.Run(func(e exam.E) {
+				env := deep.NewEnv()
+				if ok := exam.LessThan(e, env, tt.a, tt.b).Ok(); ok != tt.want {
+					t.Errorf("LessThan(%d, %d) = %v, want %v", tt.a, tt.b, ok, tt.want)
+				}
+			})
+			result := tt.matcher.Match(deep.NewEnv(), match.NewVals1(log))
+			if !result.Matched() {
+				t.Errorf("LessThan() log = %s", deep.Format(deep.NewEnv(), log))
+			}
+		})
+	}
+}
+
+func TestGreaterThan(t *testing.T) {
+	tests := []struct {
+		name    string
+		a, b    int
+		want    bool
+		matcher match.Matcher
+	}{
+		{
+			name: "a greater than b",
+			a:    7,
+			b:    5,
+			want: true,
+			matcher: match.Equal(&exam.Log{
+				Helper: true,
+			}),
+		},
+		{
+			name: "a equal to b",
+			a:    5,
+			b:    5,
+			want: false,
+			matcher: match.Pointer(match.Struct{
+				Fields: map[deep.Field]match.Matcher{
+					deep.NamedField("Error"): match.Len(match.Equal(1)),
+				},
+				Partial: match.Equal(exam.Log{
+					Helper: true,
+					Fail:   true,
+				}),
+			}),
+		},
+		{
+			name: "a less than b",
+			a:    3,
+			b:    5,
+			want: false,
+			matcher: match.Pointer(match.Struct{
+				Fields: map[deep.Field]match.Matcher{
+					deep.NamedField("Error"): match.Len(match.Equal(1)),
+				},
+				Partial: match.Equal(exam.Log{
+					Helper: true,
+					Fail:   true,
+				}),
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := exam.Harness{}
+			log := h.Run(func(e exam.E) {
+				env := deep.NewEnv()
+				if ok := exam.GreaterThan(e, env, tt.a, tt.b).Ok(); ok != tt.want {
+					t.Errorf("GreaterThan(%d, %d) = %v, want %v", tt.a, tt.b, ok, tt.want)
+				}
+			})
+			result := tt.matcher.Match(deep.NewEnv(), match.NewVals1(log))
+			if !result.Matched() {
+				t.Errorf("LessThan() log = %s", deep.Format(deep.NewEnv(), log))
+			}
+		})
+	}
+}
+
+func TestLessThanOrEqual(t *testing.T) {
+	tests := []struct {
+		name    string
+		a, b    int
+		want    bool
+		matcher match.Matcher
+	}{
+		{
+			name: "a less than b",
+			a:    3,
+			b:    5,
+			want: true,
+			matcher: match.Equal(&exam.Log{
+				Helper: true,
+			}),
+		},
+		{
+			name: "a equal to b",
+			a:    5,
+			b:    5,
+			want: true,
+			matcher: match.Equal(&exam.Log{
+				Helper: true,
+			}),
+		},
+		{
+			name: "a greater than b",
+			a:    7,
+			b:    5,
+			want: false,
+			matcher: match.Pointer(match.Struct{
+				Fields: map[deep.Field]match.Matcher{
+					deep.NamedField("Error"): match.Len(match.Equal(1)),
+				},
+				Partial: match.Equal(exam.Log{
+					Helper: true,
+					Fail:   true,
+				}),
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := exam.Harness{}
+			log := h.Run(func(e exam.E) {
+				env := deep.NewEnv()
+				if ok := exam.LessThanOrEqual(e, env, tt.a, tt.b).Ok(); ok != tt.want {
+					t.Errorf("LessThanOrEqual(%d, %d) = %v, want %v", tt.a, tt.b, ok, tt.want)
+				}
+			})
+			result := tt.matcher.Match(deep.NewEnv(), match.NewVals1(log))
+			if !result.Matched() {
+				t.Errorf("LessThanOrEqual() log = %s", deep.Format(deep.NewEnv(), log))
+			}
+		})
+	}
+}
+
+func TestGreaterThanOrEqual(t *testing.T) {
+	tests := []struct {
+		name    string
+		a, b    int
+		want    bool
+		matcher match.Matcher
+	}{
+		{
+			name: "a greater than b",
+			a:    7,
+			b:    5,
+			want: true,
+			matcher: match.Equal(&exam.Log{
+				Helper: true,
+			}),
+		},
+		{
+			name: "a equal to b",
+			a:    5,
+			b:    5,
+			want: true,
+			matcher: match.Equal(&exam.Log{
+				Helper: true,
+			}),
+		},
+		{
+			name: "a less than b",
+			a:    3,
+			b:    5,
+			want: false,
+			matcher: match.Pointer(match.Struct{
+				Fields: map[deep.Field]match.Matcher{
+					deep.NamedField("Error"): match.Len(match.Equal(1)),
+				},
+				Partial: match.Equal(exam.Log{
+					Helper: true,
+					Fail:   true,
+				}),
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := exam.Harness{}
+			log := h.Run(func(e exam.E) {
+				env := deep.NewEnv()
+				if ok := exam.GreaterThanOrEqual(e, env, tt.a, tt.b).Ok(); ok != tt.want {
+					t.Errorf("GreaterThanOrEqual(%d, %d) = %v, want %v", tt.a, tt.b, ok, tt.want)
+				}
+			})
+			result := tt.matcher.Match(deep.NewEnv(), match.NewVals1(log))
+			if !result.Matched() {
+				t.Errorf("GreaterThanOrEqual() log = %s", deep.Format(deep.NewEnv(), log))
+			}
+		})
+	}
+}
